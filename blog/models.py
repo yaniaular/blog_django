7ffc8775951297c1d2b3d94d4cc72c0e42b11d0e
Yanina Aular <yanina.aular@vauxoo.com>
2013-04-09 22:01:18 -0430

    [ADD] creadas las clases Tag, Post y Comment, con sus campos, además de ello se ejcutó python manage.py syncdb para crear sus tablas y columnas en blog_yani.db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+# clase tag, se crea una subclase de models.Model
+class Tag(models.Model):
+    # campo tag de tipo caracter
+    tag = models.CharField(max_length=100, unique=True, db_index=True)
+
+    # permite dar un nombre mas significativo a las instancias
+    def __unicode__(self):
+        return self.tag
+
+# clase post
+class Post(models.Model):
+    # relacion many to many entre Post y Tag
+    tags = models.ManyToManyField(Tag, blank=True)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+
+    #fecha de publicacion, se agrega automaticamente
+    pub_date = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+class Comment(models.Model):
+    #cada comentario pertenece a un post
+    post = models.ForeignKey(Post)
+
+    #nombre es opcional (blank) y por defecto es Anonimus (default)
+    name = models.CharField(max_length=100, blank=True, default="Anonimus")
+
+    #el cuerpo del comentario
+    body = models.TextField(verbose_name="comment")
+    date = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "Por " + self.name + " en " + self.post.title
+
+
