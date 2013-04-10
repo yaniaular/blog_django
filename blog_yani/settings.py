5f50f725fab34b5d2b70206a9e307c6fc8e71fbe
Yanina Aular <yanina.aular@vauxoo.com>
2013-04-09 21:39:45 -0430

    [ADD] creando la base de datos, se debe colocar la dirección completa donde se guardará el archivo con la base de datos .db

--- blog_yani/settings.py
+++ blog_yani/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'blog_yani',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/yanina/Django_Project/blog_yani/blog_yani.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
